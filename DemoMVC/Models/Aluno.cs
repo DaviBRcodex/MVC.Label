b451b5d14d14effb9d66ab48c24b693fcaef9ba3
Davi Projetos <g4mern4ato@gmail.com>
2024-09-28 00:38:01 -0300

    Adicionar arquivos de projeto.

---
+++ DemoMVC/Models/Aluno.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DemoMVC.Models
+{
+    public class Aluno
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Email { get; set; }
+
+        [DataType(DataType.Date)]
+
+        public DateTime? DateRegister { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
